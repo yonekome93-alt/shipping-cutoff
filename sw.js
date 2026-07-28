a5896b0b7603761b7b27fd8c7462eb034647e8b8
yonekome93-alt <yonekome93@gmail.com>
2026-07-28 19:39:45 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='shipping-cutoff-v22';
+const CACHE='shipping-cutoff-v23';
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./ship-pace-icon.png']))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))));
